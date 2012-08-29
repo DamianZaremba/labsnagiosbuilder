2f2e6fb431c4a53fb0477290dc0908fc360d79f5
Damian Zaremba <damian@damianzaremba.co.uk>
2012-08-29 11:08:17 +0100

    Adding basic tests

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 test:
 		flake8 --exclude=migrations,.git \
-					--ignore=E501,E225,E121,E123,E124,E125,E127,E128,W404 --exit-zero labsnagiosbuilder
+			--ignore=E501,E225,E121,E123,E124,E125,E127,E128,W404 \
+			--exit-zero labsnagiosbuilder || exit 1
 		cd labsnagiosbuilder && nosetests
---
+++ labsnagiosbuilder/tests/TestLdap.py
@@ -0,0 +1,112 @@
+import unittest
+from labsnagiosbuilder import build
+
+class HostInfo(unittest.TestCase):
+    def setUp(self):
+        self.instance1 = {
+        	'puppetVar': [
+        		'realm=labs',
+        		'instancecreator_email=damian@some-domain.invalid',
+        		'instancecreator_username=DamianZaremba',
+        		'instancecreator_lang=en',
+        		'instanceproject=bots',
+        		'instancename=bots-cb-dev'
+        	],
+        	'aRecord': ['10.4.0.249'],
+        	'dc': ['i-000003d1'],
+        	'puppetClass': [
+        		'base',
+        		'ldap::client::wmf-test-cluster',
+        		'exim::simple-mail-sender',
+        		'sudo::labs_project'
+        	]
+        }
+
+        self.instance2 = {
+        	'puppetVar': [
+        		'realm=labs',
+        		'instancecreator_email=damian@some-domain.invalid',
+        		'instancecreator_username=DamianZaremba',
+        		'instancecreator_lang=en',
+        		'instanceproject=bots',
+        		'instancename=bots-cb-dev-test'
+        	],
+        	'aRecord': ['10.4.0.249'],
+        	'dc': ['i-000003d1'],
+        	'puppetClass': [
+        		'base',
+        		'ldap::client::wmf-test-cluster',
+        		'webserver::apache2',
+        		'exim::simple-mail-sender',
+        		'sudo::labs_project'
+        	]
+        }
+
+    def test_groups1(self):
+    	expected_groups = ['ssh', 'bots']
+    	groups = build.get_host_groups(self.instance1)
+        self.assertEqual(groups, expected_groups)
+
+    def test_groups2(self):
+    	expected_groups = ['ssh', 'bots', 'http']
+    	groups = build.get_host_groups(self.instance2)
+        self.assertEqual(groups, expected_groups)
+
+    def test_get_puppet_vars1(self):
+    	expected_vars = {
+			'realm': 'labs',
+    		'instancecreator_email': 'damian@some-domain.invalid',
+    		'instancecreator_username': 'DamianZaremba',
+    		'instancecreator_lang': 'en',
+    		'instanceproject': 'bots',
+    		'instancename': 'bots-cb-dev'
+    	}
+    	vars = build.get_puppet_vars(self.instance1)
+        self.assertEqual(expected_vars, vars)
+
+    def test_host_info1(self):
+    	expected_host_info = {
+			'puppet_classes': [
+				'base',
+				'ldap::client::wmf-test-cluster',
+				'exim::simple-mail-sender',
+				'sudo::labs_project'
+			],
+			'fqdn': 'i-000003d1',
+			'groups': ['bots', 'ssh'],
+			'address': '10.4.0.249',
+			'puppet_vars': {
+				'realm': 'labs', 
+				'instancecreator_email': 'damian@some-domain.invalid',
+				'instancecreator_username': 'DamianZaremba',
+				'instancecreator_lang': 'en', 'instanceproject': 'bots',
+				'instancename': 'bots-cb-dev'
+			},
+			'name': 'bots-cb-dev'
+    	}
+    	host_info = build.get_host_info(self.instance1)
+        self.assertEqual(expected_host_info, host_info)
+
+    def test_host_info2(self):
+    	expected_host_info = {
+			'puppet_classes': [
+				'base',
+				'ldap::client::wmf-test-cluster',
+				'exim::simple-mail-sender',
+				'sudo::labs_project',
+				'webserver::apache2',
+			],
+			'fqdn': 'i-000003d1',
+			'groups': ['bots', 'ssh'],
+			'address': '10.4.0.249',
+			'puppet_vars': {
+				'realm': 'labs', 
+				'instancecreator_email': 'damian@some-domain.invalid',
+				'instancecreator_username': 'DamianZaremba',
+				'instancecreator_lang': 'en', 'instanceproject': 'bots',
+				'instancename': 'bots-cb-dev-test'
+			},
+			'name': 'bots-cb-dev-test'
+    	}
+    	host_info = build.get_host_info(self.instance2)
+        self.assertEqual(expected_host_info, host_info)
